5d8996c0265ecd83c7fb649c01189f175cde9339
eugenyProchan <pro4anjey@gmail.com>
2020-06-22 10:25:11 +0500

    add package_1 dependency

---
+++ my_package_2/main.py
@@ -0,0 +1,1 @@
+message = 'Hello from mypackage_2'
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyPackageName_2',
+    version='1.0.0',
+    url='https://github.com/mypackage.git',
+    author='Author Name',
+    author_email='author@gmail.com',
+    description='Description of the package',
+    packages=find_packages(),
+    install_requires=[
+        'requests >= 2.24.0',
+        'MyPackageName_1 @ git+https://git@github.com/eugenyProchan/setup_lesson_1@master#egg=MyPackageName_1',
+    ]
+)
